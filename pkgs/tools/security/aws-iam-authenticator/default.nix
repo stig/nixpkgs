565fbeb8a5f387bfcb8900f4d4de12bf7cc48e96
Stig Brautaset <stig@brautaset.org>
2021-06-09 21:20:11 +0100

    Revert "aws-iam-authenticator: 0.5.2 -> 0.5.3"

--- pkgs/tools/security/aws-iam-authenticator/default.nix
+++ pkgs/tools/security/aws-iam-authenticator/default.nix
@@ -2,18 +2,16 @@
 
 buildGoModule rec {
   pname = "aws-iam-authenticator";
-  version = "0.5.3";
+  version = "0.5.2";
 
   src = fetchFromGitHub {
     owner = "kubernetes-sigs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0ga3vf5gn7533iqnxn7kchb6xg5wvk92livlqzkhi5qvqhl1sbw0";
+    sha256 = "1xfc3a0dik4930va30sbl6687v6i5irv96fwis9lrqh5rjqnblqp";
   };
 
-  # Upstream has inconsistent vendoring, see https://github.com/kubernetes-sigs/aws-iam-authenticator/issues/377
-  deleteVendor = true;
-  vendorSha256 = "+Z8sENIMWXP29Piwb/W6i7UdNXVq6ZnO7AZbSaUYCME=";
+  vendorSha256 = null;
 
   buildFlagsArray = [ "-ldflags=-s -w -X main.version=v${version}" ];
 
